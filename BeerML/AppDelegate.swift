f2efab76a8593df5bb60c4d3c9624805fee3dbc3
kawer <kawer@Lazaros-MacBook-Pro.local>
2018-09-27 18:30:47 -0500

    first commit

---
+++ BeerML/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BeerML
+//
+//  Created by Lazaro Kawer on 9/27/18.
+//  Copyright © 2018 Lazaro Kawer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
